456adc1e0e6291ff1242f5d974d33796f70d0ee6
DyadyaNadya <vortexshark228@gmail.com>
2024-10-26 13:40:18 +0500

    Добавьте файлы проекта.

---
+++ Lab 2.2-2.3/Program.cs
@@ -0,0 +1,59 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+
+        try
+        {
+            //Конструктор по умолчанию
+            RightTriangle triangle1 = new RightTriangle();
+            Console.WriteLine("Первый треугольник:");
+            Console.WriteLine(triangle1.ToString());
+
+            // Конструктор копирования
+            RightTriangle triangle2 = new RightTriangle(triangle1);
+            Console.WriteLine("\nКопия первого треугольника:");
+            Console.WriteLine(triangle2.ToString());
+
+            // Конструктор с параметрами 
+            Console.WriteLine("\nВведите длину катета A:");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите длину катета B:");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            RightTriangle userTriangle = new RightTriangle(a, b);
+            Console.WriteLine("\nТреугольник, созданный пользователем:");
+            Console.WriteLine(userTriangle.ToString());
+
+            // Тестирование унарных операций
+            RightTriangle incrementedTriangle = ++triangle1;
+            Console.WriteLine("\nТреугольник 1 после увеличения сторон в 2 раза:");
+            Console.WriteLine(incrementedTriangle.ToString());
+
+            RightTriangle decrementedTriangle = --triangle2;
+            Console.WriteLine("\nТреугольник 2 после уменьшения сторон в 2 раза:");
+            Console.WriteLine(decrementedTriangle.ToString());
+
+            // Тестирование приведения типа к double
+            double area = (double)triangle1;
+            Console.WriteLine($"\nПлощадь первого треугольника: {area}");
+
+            // Тестирование приведения типа к bool
+            bool exists = triangle1;
+            Console.WriteLine($"\nСуществует ли треугольник, введенный пользователем: {exists}");
+
+            // Тестирование бинарных операций сравнения
+
+            bool isLessOrEqual = triangle1 <= triangle2;
+            bool isGreaterOrEqual = triangle1 >= userTriangle;
+            Console.WriteLine($"\nПервый треугольник <= Второй треугольник: {isLessOrEqual}");
+            Console.WriteLine($"Первый треугольник >= Пользовательский треугольник: {isGreaterOrEqual}");
+        }
+        catch
+        {
+            Console.WriteLine("Ошибка: нужно ввести положительное число!");
+        }
+    }
+}
+
---
+++ Lab 2.2-2.3/RightTriangle.cs
@@ -0,0 +1,99 @@
+public class RightTriangle
+{
+    protected double a;
+    protected double b;
+
+    public double A
+    {
+        get { return a; }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Длина катета должна быть положительным числом.");
+            }
+            a = value;
+        }
+    }
+
+    public double B
+    {
+        get { return b; }
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Длина катета должна быть положительным числом.");
+            }
+            b = value;
+        }
+    }
+
+    // Конструктор с параметрами
+    public RightTriangle(double a, double b)
+    {
+        A = a;
+        B = b;
+    }
+
+    //Конструктор по умолчанию
+    public RightTriangle()
+    {
+        A = 2;
+        B = 3;
+    }
+    // Конструктор копирования
+    public RightTriangle(RightTriangle other)
+    {
+        A = other.A + 2;
+        B = other.B + 5;
+    }
+
+    // Метод для вычисления площади треугольника
+    public double CalculateArea()
+    {
+        return (A * B) / 2;
+    }
+
+    //Перегрузка оператора ++
+    public static RightTriangle operator ++(RightTriangle triangle)
+    {
+        return new RightTriangle(triangle.A * 2, triangle.B * 2);
+    }
+
+    //Перегрузка оператора --
+    public static RightTriangle operator --(RightTriangle triangle)
+    {
+        return new RightTriangle(triangle.A / 2, triangle.B / 2);
+    }
+
+    public static explicit operator double(RightTriangle triangle)
+    {
+        return triangle.CalculateArea();
+    }
+
+    public static implicit operator bool(RightTriangle triangle)
+    {
+        return triangle.A > 0 && triangle.B > 0;
+    }
+
+    // Бинарные операции для сравнения площадей треугольников
+    public static bool operator <=(RightTriangle t1, RightTriangle t2)
+    {
+        return t1.CalculateArea() <= t2.CalculateArea();
+    }
+
+    public static bool operator >=(RightTriangle t1, RightTriangle t2)
+    {
+        return t1.CalculateArea() >= t2.CalculateArea();
+    }
+    // Перегрузка метода ToString
+    public override string ToString()
+    {
+        return $"Катет A: {A}, Катет B: {B}, Площадь: {CalculateArea()}";
+    }
+}
+
+
+
+
